a935383aa3b6c85b5e5d8ec41a01f6cc974ca066
Jochen Just <jochen.just@novatec-gmbh.de>
2023-03-20 16:31:28 +0100

    Uses refreshVersions plugin to manage dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,20 @@
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm")
+}
+
+repositories {
+    // without specifying at least one repository
+    // refreshVersions will fail with:
+    // "Cannot get version candidates with an empty fetchers list."
+    mavenCentral()
 }
 
 dependencies {
-    implementation("org.flywaydb:flyway-core:9.11.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
-    testImplementation("org.assertj:assertj-core:3.12.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+    // The '_' is a necessary placeholder for refreshVersions to work
+    implementation("org.flywaydb:flyway-core:_")
+    // For well known dependencies, it even offers type safe objects.
+    // They will help you in your IDE via auto-completion.
+    testImplementation(Testing.junit.jupiter.api)
+    testImplementation(Testing.assertj.core)
+    testRuntimeOnly(Testing.junit.jupiter.engine)
 }
-
